16bbdcaa2662dcab151db1049b734c04e9a783a6
tube22-ex <79168960+tube22-ex@users.noreply.github.com>
2024-01-21 01:05:51 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const contents = document.getElementById('contents');
+like_data.forEach((d)=>{
+    contents.insertAdjacentHTML('beforeend',`
+    <div class="item">
+        <a href='${d["URL"]}' target='_blank'>
+            <div class="info">
+                <span class="star">☆${d["STAR"]}</span>
+                <span class="level">?:${d["LEVEL"]}</span>
+                <span class="len">文字数:${d["LEN"]}</span>
+                <span class="avg">平均:${d["AVG"]}</span>
+            </div>
+            <img src='${d["IMG"]}'>
+            <p class="title">${d["TITLE"]}</p>
+        <a href='${d["URL"]}'>
+    </div>
+    
+    `)
+})
